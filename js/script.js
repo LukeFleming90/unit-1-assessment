55b07cb649c4cade17b5a611391756e198a7bd2b
Luke Fleming <lukefleming@Lukes-MBP.fios-router.home>
2022-02-02 20:58:14 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ plusButton.addEventListener('click', (evt) => {
     let currentNumberIterator = parseInt(document.getElementById('number-counter').value);
     let displayNumberUse = parseInt(displayNumberText.innerText);
     displayNumberUse += currentNumberIterator
-    displayNumberText.innerText = displayNumberUse
+    if(displayNumberUse < 0) {
+        displayNumberText.classList.add('red')
+    } else {
+        displayNumberText.classList.remove('red')
+    }
+    displayNumberText.innerText = displayNumberUse;
 });
 
 minusButton.addEventListener('click', (evt) => {
     let currentNumberIterator = parseInt(document.getElementById('number-counter').value);
     let displayNumberUse = parseInt(displayNumberText.innerText);
-    displayNumberUse -= currentNumberIterator
-    displayNumberText.innerText = displayNumberUse
+    displayNumberUse -= currentNumberIterator;
+    if(displayNumberUse < 0) {
+        displayNumberText.classList.add('red')
+    } else {
+        displayNumberText.classList.remove('red')
+    }
+    displayNumberText.innerText = displayNumberUse;
 });
